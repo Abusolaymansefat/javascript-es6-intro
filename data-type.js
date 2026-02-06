bbf5b0276df6b12bf8edc7c8448b7b4f25c7eeca
Abu Solayman Sefat <sefat01625@gmail.com>
2026-02-06 21:27:00 +0600

    explain the primitive data & non primitive data types

---
+++ data-type.js
@@ -0,0 +1,29 @@
+// js is a dynamically typed programming language,
+
+let data = 6;
+
+data = "hollo world";
+data = false;
+
+data = { name: "john", age: 30}
+
+data = [5565];
+data = undefined;
+data= null;
+
+console.log(typeof data);
+
+
+//js: primitives data types: string, number, boolean, undefined, null, symbol, bigint
+
+
+//js : non primitive data types: objects, arrays, functions, null
+
+let money;
+console.log(money); //undefiend
+
+function total (a, b){
+          console.log('value of a parametier ', a, b)
+}
+
+total()

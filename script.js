edc26369f09bc4a06db4fa6ef6485427ce0d0a37
Abu Solayman Sefat <sefat01625@gmail.com>
2026-02-09 23:06:01 +0600

    json data post

--- script.js
+++ script.js
@@ -1,25 +1,74 @@
-console.log("json is a data format");
+const loadData = () => {
+          fetch("https://jsonplaceholder.typicode.com/todos/1")
+                    // promise of response
+                    .then((res) => res.json())
+                    .then((data) => console.log(data))
+};
 
-const person = {
-          name: ' habib',
-          age: 20,
-          isRich: false,
-          hobbies: ['coding','gaming', 'travelling'],
-          address: {
-                    city: 'dhaka',
-                    country: 'bangladesh'
-          },
-          money: 30000,
+
+const loadPosts = () => {
+          const url = "https://jsonplaceholder.typicode.com/posts"
+          fetch(url)
+                    .then(res => res.json())
+                    .then(data => {
+                              // console.log(data)
+                              displayPost(data)
+                    })
 }
 
-console.log('person object', person, typeof person);
+const displayPost = (posts) => {
+
+          const postContainer = document.getElementById('post-list');
+          // console.log(postContainer)
+          // posts.forEach((post) => {
+          //           console.log(post)
+          // })
+
+          // for (let i = 0 ; i < posts.length; i++){
+          //           console.log(posts[i])
+          // }
+
+          // for(const post of posts){
+          //           console.log(post)
+          // }
 
 
-// JSON -> javascript object notation
+          posts.forEach((post) => {
+                    console.log(post.title);
+                    //  create HTML element
 
-const personJSON = JSON.stringify(person);
-console.log('person json', personJSON, typeof personJSON);
+                    const li = document.createElement('li');
+                    li.innerText = post.title;
+                    console.log(li);
+                    postContainer.appendChild(li)
+          })
+}
+
+// console.log('result of fetch', result)
 
 
-const personObj = JSON.parse(personJSON);
-// console.log(personJSON);
+// console.log("json is a data format");
+
+// const person = {
+//           name: ' habib',
+//           age: 20,
+//           isRich: false,
+//           hobbies: ['coding','gaming', 'travelling'],
+//           address: {
+//                     city: 'dhaka',
+//                     country: 'bangladesh'
+//           },
+//           money: 30000,
+// }
+
+// console.log('person object', person, typeof person);
+
+
+// // JSON -> javascript object notation
+
+// const personJSON = JSON.stringify(person);
+// console.log('person json', personJSON, typeof personJSON);
+
+
+// const personObj = JSON.parse(personJSON);
+// // console.log(personJSON);
